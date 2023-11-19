48ccda79ec4bef9cadf9689779a17636af421e40
somethingbear <clq@outlook.it>
2023-11-19 20:33:47 +0800

    feat; nnr image

--- src/app/nnr-transfer/page.tsx
+++ src/app/nnr-transfer/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { Button, Input, message } from "antd";
+import { Button, Input, QRCode, Tooltip, message, Image } from "antd";
 import React, { useMemo, useState } from "react";
 import copy from "copy-to-clipboard";
-import { QRCodeSVG } from "qrcode.react";
-import { CopyOutlined } from "@ant-design/icons";
+import { CopyOutlined, InfoCircleOutlined } from "@ant-design/icons";
+import Link from "next/link";
+// import Image from "next/image";
 
 const base64Encode = (str: string) => {
   return Buffer.from(str, "utf8").toString("base64");
@@ -18,6 +19,7 @@ const NNRTransfer = () => {
   // vmess://eyJ2IjogIjIiLCAicHMiOiAiOWNsb3VkLnZpcC0zOC4xNDMuMTguMTYyIiwgImFkZCI6ICJ1cy1zZC1oeS5oYXBweWNhdDEyLmNvbSIsICJwb3J0IjogMjU5OTcsICJpZCI6ICIzNTM2ZDlmNy1jOGJhLTQ4YTItODllMi05OTBlMmEyOGNmNDgiLCAiYWlkIjogIjAiLCAic2N5IjogImF1dG8iLCAibmV0IjogInRjcCIsICJ0eXBlIjogIm5vbmUiLCAiaG9zdCI6ICIiLCAicGF0aCI6ICIiLCAidGxzIjogIm5vbmUiLCAic25pIjogIiIsICJhbHBuIjogIiJ9
   const [originalVmessStr, setOriginalVmessStr] = useState<string>("");
   const [hostPortNNR, setHostPortNNR] = useState<string>("");
+  const [showImage, setShowImage] = useState(0);
 
   const originalVmessJson = useMemo(() => {
     try {
@@ -49,9 +51,22 @@ const NNRTransfer = () => {
 
   return (
     <main className="p-6 flex flex-col gap-4">
-      <h2>NNR Transfer</h2>
+      <h2>
+        <Link href={"https://nnr.moe"} target="_blank" className=" underline text-blue-500">
+          NNR
+        </Link>{" "}
+        Transfer
+      </h2>
+
       <section>
-        Paste Original vmess Here
+        <span className="flex items-center gap-4">
+          Paste Original vmess Here
+          <InfoCircleOutlined
+            onClick={() => {
+              setShowImage(1);
+            }}
+          />
+        </span>
         <Input
           value={originalVmessStr}
           onChange={(e) => {
@@ -60,7 +75,14 @@ const NNRTransfer = () => {
         />
       </section>
       <section>
-        {`Copy to NNR Host:`}
+        <span className="flex items-center gap-4">
+          Copy to NNR Host:
+          <InfoCircleOutlined
+            onClick={() => {
+              setShowImage(2);
+            }}
+          />
+        </span>
         <Input
           readOnly
           value={originalVmessJson?.add}
@@ -77,7 +99,14 @@ const NNRTransfer = () => {
         />
       </section>
       <section>
-        {`Copy to NNR Port:`}
+        <span className="flex items-center gap-4">
+          Copy to NNR Port:
+          <InfoCircleOutlined
+            onClick={() => {
+              setShowImage(2);
+            }}
+          />
+        </span>
         <Input
           readOnly
           value={originalVmessJson?.port}
@@ -95,7 +124,14 @@ const NNRTransfer = () => {
       </section>
 
       <section>
-        {`Past "HOST:PORT" from NNR`}
+        <span className="flex items-center gap-4">
+          {`Paste "HOST:PORT" from NNR`}
+          <InfoCircleOutlined
+            onClick={() => {
+              setShowImage(3);
+            }}
+          />
+        </span>
         <Input
           value={hostPortNNR}
           onChange={(e) => {
@@ -123,7 +159,25 @@ const NNRTransfer = () => {
           }
         />
       </section>
-      <section>{newVmessStr && <QRCodeSVG value={newVmessStr} />}</section>
+      <section>{newVmessStr && <QRCode value={newVmessStr} />}</section>
+      <section>
+        <Image
+          src={`/images/nnr-transfer/${showImage}.png`}
+          width={200}
+          preview={{
+            visible: showImage > 0,
+            onVisibleChange: (value) => {
+              if (!value) {
+                setShowImage(0);
+              }
+            },
+          }}
+          alt=""
+          className="hidden"
+
+          // style={{ display: "none" }}
+        />
+      </section>
     </main>
   );
 };
