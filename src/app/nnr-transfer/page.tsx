195f73ee149b0ceab2a70972ce4bcaef35e28d2c
somethingbear <clq@outlook.it>
2024-07-09 18:25:09 +0800

    feat: 增加国家备注; 支持ss

--- src/app/nnr-transfer/page.tsx
+++ src/app/nnr-transfer/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { Button, Input, QRCode, Tooltip, message, Image } from "antd";
+import { Button, Input, QRCode, Tooltip, message, Image, Select } from "antd";
 import React, { useMemo, useState } from "react";
 import copy from "copy-to-clipboard";
 import {
   CopyOutlined,
+  EnterOutlined,
   InfoCircleOutlined,
   QuestionCircleOutlined,
 } from "@ant-design/icons";
@@ -21,17 +22,37 @@ const base64Decode = (str: string) => {
 
 const NNRTransfer = () => {
   // vmess://eyJ2IjogIjIiLCAicHMiOiAiOWNsb3VkLnZpcC0zOC4xNDMuMTguMTYyIiwgImFkZCI6ICJ1cy1zZC1oeS5oYXBweWNhdDEyLmNvbSIsICJwb3J0IjogMjU5OTcsICJpZCI6ICIzNTM2ZDlmNy1jOGJhLTQ4YTItODllMi05OTBlMmEyOGNmNDgiLCAiYWlkIjogIjAiLCAic2N5IjogImF1dG8iLCAibmV0IjogInRjcCIsICJ0eXBlIjogIm5vbmUiLCAiaG9zdCI6ICIiLCAicGF0aCI6ICIiLCAidGxzIjogIm5vbmUiLCAic25pIjogIiIsICJhbHBuIjogIiJ9
-  const [originalVmessStr, setOriginalVmessStr] = useState<string>("");
+  const [originalStr, setOriginalStr] = useState<string>("");
   const [hostPortNNR, setHostPortNNR] = useState<string>("");
   const [showImage, setShowImage] = useState(0);
 
+  const [country, setCountry] = useState("美国");
+
   const originalVmessJson = useMemo(() => {
+    if (!originalStr) return null;
+    if (!originalStr.startsWith("vmess://")) return null;
     try {
-      return JSON.parse(base64Decode(originalVmessStr.split("://")[1]));
+      return JSON.parse(base64Decode(originalStr.split("://")[1]));
     } catch (e) {
       return null;
     }
-  }, [originalVmessStr]);
+  }, [originalStr]);
+
+  const extractHostPort = (ssString: string): string | null => {
+    const regex = /@([^#]+)#/;
+    const matches = ssString.match(regex);
+    return matches ? matches[1] : null;
+  };
+
+  const originalSSJson = useMemo(() => {
+    if (!originalStr) return null;
+    if (!originalStr.startsWith("ss://")) return null;
+    const hostPort = extractHostPort(originalStr);
+    return {
+      host: hostPort ? hostPort.split(":")[0] : "",
+      port: hostPort ? hostPort.split(":")[1] : "",
+    };
+  }, [originalStr]);
 
   const newVmessJson = useMemo(() => {
     if (!originalVmessJson) {
@@ -46,14 +67,33 @@ const NNRTransfer = () => {
       ...originalVmessJson,
       add: host,
       port: port,
+      ps: `${country ? `${country}-` : ""}${originalVmessJson.ps}`,
     };
-  }, [hostPortNNR, originalVmessJson]);
+  }, [hostPortNNR, originalVmessJson, country]);
 
   const newVmessStr = useMemo(() => {
     return newVmessJson
       ? `vmess://${base64Encode(JSON.stringify(newVmessJson))}`
       : "";
   }, [newVmessJson]);
+
+  const extractPreHostPort =(ssString: string): string | null => {
+    const regex = /^ss:\/\/([^@]+)/;
+    const matches = ssString.match(regex);
+    return matches ? matches[1] : null;
+  }
+
+  const newSSStr = useMemo(() => {
+    const cipherPassword = extractPreHostPort(originalStr);
+    if (!cipherPassword) {
+      return "";
+    }
+    const [host, port] = hostPortNNR.split(":");
+    if (!host || !port) {
+      return "";
+    }
+    return `ss://${cipherPassword}@${host}:${port}#${country ? `${country}-` : ""}${originalSSJson?.host}`;
+  }, [originalStr, originalSSJson, hostPortNNR, country])
 
   return (
     <main className="flex flex-col gap-4 p-6">
@@ -70,28 +110,30 @@ const NNRTransfer = () => {
 
       <div className="flex flex-col gap-2">
         <span
-          className="underline"
+          className="cursor-pointer text-blue-300 underline"
           onClick={() => {
             copy("starfly.online:");
             message.success("Copied");
           }}
         >
           starfly.online
+          <CopyOutlined />
         </span>
         <span
-          className="underline"
+          className="cursor-pointer text-blue-300 underline"
           onClick={() => {
             copy("ninestar.online:");
             message.success("Copied");
           }}
         >
           ninestar.online
+          <CopyOutlined />
         </span>
       </div>
 
       <section>
         <span className="flex items-center gap-4">
-          <span>Copy&Paste Original Vmess Here</span>
+          <span>Copy&Paste Original Here</span>
           <span
             className="flex cursor-pointer items-center gap-[8px] text-gray-400"
             onClick={() => {
@@ -103,9 +145,9 @@ const NNRTransfer = () => {
           </span>
         </span>
         <Input
-          value={originalVmessStr}
+          value={originalStr}
           onChange={(e) => {
-            setOriginalVmessStr(e.target.value);
+            setOriginalStr(e.target.value);
           }}
         />
       </section>
@@ -124,7 +166,7 @@ const NNRTransfer = () => {
         </span>
         <Input
           readOnly
-          value={originalVmessJson?.add}
+          value={originalVmessJson?.add || originalSSJson?.host}
           disabled
           suffix={
             <CopyOutlined
@@ -152,7 +194,7 @@ const NNRTransfer = () => {
         </span>
         <Input
           readOnly
-          value={originalVmessJson?.port}
+          value={originalVmessJson?.port || originalSSJson?.port}
           disabled
           suffix={
             <CopyOutlined
@@ -178,6 +220,24 @@ const NNRTransfer = () => {
             How
             <QuestionCircleOutlined />
           </span>
+          <span
+            className="cursor-pointer text-blue-300 underline"
+            onClick={() => {
+              setHostPortNNR("starfly.online:");
+            }}
+          >
+            starfly.online
+            <EnterOutlined />
+          </span>
+          <span
+            className="cursor-pointer text-blue-300 underline"
+            onClick={() => {
+              setHostPortNNR("ninestar.online:");
+            }}
+          >
+            ninestar.online
+            <EnterOutlined />
+          </span>
         </span>
         <Input
           value={hostPortNNR}
@@ -190,23 +250,43 @@ const NNRTransfer = () => {
       {/* {JSON.stringify(newVmessJson)} */}
 
       <section>
-        New Vmess:
+        <span className="flex">Country:</span>
+        <Select
+          className="w-[250px]"
+          value={country}
+          onChange={(value) => {
+            setCountry(value);
+          }}
+          allowClear
+        >
+          <Select.Option value="美国">美国</Select.Option>
+          <Select.Option value="英国">英国</Select.Option>
+          <Select.Option value="泰国">泰国</Select.Option>
+          <Select.Option value="越南">越南</Select.Option>
+          <Select.Option value="新加坡">新加坡</Select.Option>
+          <Select.Option value="马来西亚">马来西亚</Select.Option>
+          <Select.Option value="菲律宾">菲律宾</Select.Option>
+        </Select>
+      </section>
+
+      <section>
+        New:
         <Input
-          value={newVmessStr}
+          value={newVmessStr || newSSStr}
           readOnly
           disabled
           suffix={
             <CopyOutlined
               onClick={() => {
-                if (!newVmessStr) return;
-                copy(newVmessStr);
+                if (!newVmessStr || !newSSStr) return;
+                copy(newVmessStr || newSSStr);
                 message.success("Copied");
               }}
             />
           }
         />
       </section>
-      <section>{newVmessStr && <QRCode value={newVmessStr} />}</section>
+      <section>{(newVmessStr || newSSStr) && <QRCode value={newVmessStr || newSSStr} />}</section>
       <section>
         <Image
           src={`/images/nnr-transfer/${showImage}.png`}
